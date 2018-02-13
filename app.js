d5342783d5b36b8e9a6cac0fbae84a98da9a1579
Teo Almonte <teoalmonte@gmail.com>
2018-02-12 23:52:05 -0500

    all

--- app.js
+++ app.js
@@ -5,17 +5,43 @@ const express = require('express')
 const mongoose = require('mongoose')
 const auth = require('./routes/auth');
 const passport = require('passport')
+const keys = require('./config/keys')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 /* ********************
  * Middleware
  * ******************** */
+// Initiate App
+const app = express();
+
 // Run Mongoose
+mongoose.connect(keys.mongoURI)
+  .then(() => console.log('running on mongo'))
+  .catch(err => console.log(err))
+
+// User Model
+require('./models/User')
+
+// Cookies
+app.use(cookieParser())
+
+// Sessions
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
 
 // Passport
 require('./config/passport')(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
-// Initiate App
-const app = express();
+app.use((req, res, next) => {
+  res.locals.user = req.user || null
+  next()
+})
 
 /* ********************
  * Routes
